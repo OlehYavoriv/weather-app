021dd3cd2e6615a1adbcdb9bfb58d88656a7f5a3
OlehYavoriv <oleh.yavoriv@gmail.com>
2023-10-12 22:45:02 +0300

    create IntroPage

--- src/routes/Routing.tsx
+++ src/routes/Routing.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import { IntroPage } from "../pages/IntroPage";
 
 export const Routing = () => {
     return (
         <Routes>
-            <Route path='/' element={<p>Weather</p>}/>
+            <Route path='/' element={<IntroPage/>}/>
+            <Route path='/weather' element={<p>Weather</p>}/>
         </Routes>
     );
 };
